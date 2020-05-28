134edf4a559294cfba0bdda7da13de06965ea0f3
xiaojie123 <1135319553@qq.com>
2020-05-28 17:56:21 +0800

    数据库切换回最后的版本

--- config/config.default.js
+++ config/config.default.js
@@ -61,7 +61,7 @@ module.exports = appInfo => {
   // 数据库配置
   config.mongoose = {
     client: {
-      url: 'mongodb://admin:admin123@59.110.162.236:27017:27017/Design?authSource=admin', 
+      url: 'mongodb://admin:admin123@59.110.162.236:27017:27017/lastone?authSource=admin', 
       options: {
         useNewUrlParser: true,
       },
